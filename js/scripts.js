d52820a44033381f391b18270e8b7ae98a0b4fb8
kangogopeter <48472123+kangogopeter@users.noreply.github.com>
2019-04-11 16:17:08 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,29 @@ $(document).ready(function(){
     });
 });
 
+var toSlide = 1;
+showSlides(toSlide);
 
+function plusSlides(pet) {
+  showSlides(toSlide += pet);
+}
+
+function currentSlide(pet) {
+  showSlides(toSlide = pet);
+}
+
+function showSlides(pet) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (pet > slides.length) {toSlide = 1}    
+  if (pet < 1) {toSlide = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[toSlide-1].style.display = "block";  
+  dots[toSlide-1].className += " active";
+}
